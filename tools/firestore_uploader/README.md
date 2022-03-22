59b63096b162bcde5007fb4948c3b248925b443f
mayank rai <mayank271993@gmail.com>
2022-03-22 23:10:12 +0100

    Adds firestore uploader

---
+++ tools/firestore_uploader/README.md
@@ -0,0 +1,19 @@
+# Purpose
+
+CLI tool for moving data in-n-out of [Cloud Firestore](https://firebase.google.com/docs/firestore/).
+
+# Install
+
+- Clone and run `npm install`
+- Download the service account from your Firebase project settings, then save it as `credentials.json` in the project root.
+- `npm run build` and you're off and running.
+
+# Usage
+
+## Update data in firestore
+
+### Command templates 
+`npm run migrate i <flavor> <filename>`  
+
+### Examples 
+` npm run migrate i kanto pokedex_kanto.json`
---
+++ tools/firestore_uploader/src/index.ts
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+import 'source-map-support/register'
+import * as admin from 'firebase-admin';
+import { Command } from 'commander';
+
+const program = new Command();
+program.version('0.4.0');
+
+var flavor = process.argv[3];
+var serviceAccount = require('../data/' + flavor + '/credentials.json');
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+import * as importCollection from './importCollection';
+
+program.command('import <flavor> <file>')
+    .alias('i')
+    .arguments('[collections...]')
+    .option('-a, --auto-id [str]', 'Document ID token specifying auto generated Document ID.', 'Auto-ID')
+    .option('-m, --merge', 'Merge Firestore documents. Default is Replace.')
+    .option('-p, --coll-prefix [prefix]', '(Sub-)Collection prefix.', 'collection')
+    .action((_, fileName, collections, options) => {
+        var collectionsToImportFilePath = 'data/' + flavor + '/' + fileName;
+        importCollection.execute(collectionsToImportFilePath, collections, options);
+    });
+
+program.parse();
